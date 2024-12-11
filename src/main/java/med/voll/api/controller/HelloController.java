0a250c508b9767284422fa71c4bc4953f98593b6
juanse951 <juanse951@gmail.com>
2024-12-10 21:08:27 -0500

    se crea el payload en imsonmia con el localhost/medicos, pasando un post con un un un body JSON : de datos de medicos

--- src/main/java/med/voll/api/controller/HelloController.java
+++ src/main/java/med/voll/api/controller/HelloController.java
@@ -8,9 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloController {
 
+
     @GetMapping
     public String helloWorld(){
-        return "Hello world!";
+        return "Hello world from Europe!";
     }
 
 }

9c64e604201960c29825fefdf94b02cd31215c62
juanse951 <juanse951@gmail.com>
2024-12-11 19:23:28 -0500

    mapeamos los datos del Json insomnia en record y enum, osea los volvemos objetos java

---
+++ src/main/java/med/voll/api/DatosDireccion.java
@@ -0,0 +1,17 @@
+package med.voll.api;
+
+public record DatosDireccion(
+
+        String calle,
+
+        String distrito,
+
+        String ciudad,
+
+        String numero,
+
+        String complemento
+
+
+) {
+}
---
+++ src/main/java/med/voll/api/medico/DatosRegistroMedico.java
@@ -0,0 +1,16 @@
+package med.voll.api.medico;
+
+import med.voll.api.DatosDireccion;
+
+public record DatosRegistroMedico(
+        String nombre,
+
+        String email,
+
+        String documento,
+
+        Especialidad especialidad,
+
+        DatosDireccion direccion
+) {
+}

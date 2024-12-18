4bde913f81bbff12e9ca37ac745f9882a4854d13
juanse951 <juanse951@gmail.com>
2024-12-18 09:08:03 -0500

    Se agrega un nuevo método en el Controller del paciente

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -6,10 +6,10 @@ import med.voll.api.paciente.DatosRegistroPaciente;
 import med.voll.api.paciente.Paciente;
 import med.voll.api.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/pacientes")
@@ -22,5 +22,10 @@ public class PacienteController {
     @Transactional
     public void registrar(@RequestBody @Valid DatosRegistroPaciente datos){
         repository.save(new Paciente(datos));
+    }
+
+    @GetMapping
+    public Page<DatosListaPaciente> listar(@PageableDefault(page=0, size = 10, sort = {"nombre"})Pageable paginacion){
+        return repository.findAll(paginacion).map(DatosListaPaciente)
     }
 }

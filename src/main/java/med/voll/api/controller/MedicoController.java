f3973c4ba2c8f099b080d9aa8c95a2e329b7e08c
juanse951 <juanse951@gmail.com>
2024-12-17 23:36:47 -0500

    con @PageableDefault podemos sobre escribir los valores por defecto de Page, pero si los definimos en el queryParams sobreEscribe los ya definidos en @PageableDefault.

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -8,6 +8,7 @@ import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,7 +25,7 @@ public class MedicoController {
         medicoRepository.save(new Medico(datosRegistroMedico));
     }
     @GetMapping
-    public Page<DatosListadoMedico> listadoMedicos(Pageable paginacion){
+    public Page<DatosListadoMedico> listadoMedicos(@PageableDefault(size = 2) Pageable paginacion){
         return medicoRepository.findAll(paginacion)
                 .map(DatosListadoMedico::new);
     }

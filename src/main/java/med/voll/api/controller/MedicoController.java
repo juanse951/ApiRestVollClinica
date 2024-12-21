28e7ed7062ee57c5c4f75e38dd5b168a52715da9
juanse951 <juanse951@gmail.com>
2024-12-21 09:29:17 -0500

    mejoramos el metodo post de controller, pero como no es buena practica retornar directamente la entidad (medico) retornamos DatoRespuestaMédico(dto con los valores de la entidad medico) y establecemos en el metodo del controller uno a uno los valores que debería tener nuestro médico para retornar en el metodo.// cambiamos del void a responseEntity y agregamos el valor UriComponentsBuilder , para crear y retornar una url dinamica//al response entity le agregamos el parametro DatosRespuestaMedico para que solo pueda trabajar con ese DTO esto es buena practica/// recibimos asi el 201 created

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -10,7 +10,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,8 +23,16 @@ public class MedicoController {
     private MedicoRepository medicoRepository;
 
     @PostMapping
-    public void registrarMedico(@RequestBody @Valid DatosRegistroMedico datosRegistroMedico){
-        medicoRepository.save(new Medico(datosRegistroMedico));
+    public ResponseEntity<DatosRespuestaMedico> registrarMedico(@RequestBody @Valid DatosRegistroMedico datosRegistroMedico ,
+                                          UriComponentsBuilder uriComponentsBuilder){
+        Medico medico = medicoRepository.save(new Medico(datosRegistroMedico));
+        DatosRespuestaMedico datosRespuestaMedico = new DatosRespuestaMedico(medico.getId(), medico.getNombre(), medico.getEmail(),
+                medico.getTelefono(), medico.getEspecialidad().toString(),
+                new DatosDireccion(medico.getDireccion().getCalle(),medico.getDireccion().getDistrito(),
+                        medico.getDireccion().getCiudad(),medico.getDireccion().getNumero(),
+                        medico.getDireccion().getComplemento()));
+        URI url = uriComponentsBuilder.path("/medicos/{id}").buildAndExpand(medico.getId()).toUri();
+        return ResponseEntity.created(url).body(datosRespuestaMedico);
     }
     @GetMapping
     public Page<DatosListadoMedico> listadoMedicos(@PageableDefault(size = 12) Pageable paginacion){

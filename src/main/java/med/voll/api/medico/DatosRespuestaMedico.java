36c9b897703a0b125908e915280c3a89420c3602
juanse951 <juanse951@gmail.com>
2024-12-21 08:56:28 -0500

    mejoramos el metodo put de controller, pero como no es buena practica retornar directamente la entidad (medico) creamos un nuevo DatoRespuestaMédico(dto con los valores de la entidad medico) y establecemos en el metodo del controller uno a uno los valores que debería tener nuestro médico para retornar en el metodo.//

---
+++ src/main/java/med/voll/api/medico/DatosRespuestaMedico.java
@@ -0,0 +1,18 @@
+package med.voll.api.medico;
+
+import med.voll.api.direccion.DatosDireccion;
+
+public record DatosRespuestaMedico(
+
+        Long id,
+
+        String nombre,
+
+        String email,
+
+        String telefono,
+
+        String documento,
+
+        DatosDireccion direccion) {
+}

9ac16c173930b9a58ad6ce096cdfe5fead9fde7c
juanse951 <juanse951@gmail.com>
2024-12-23 17:42:04 -0500

    se implemente en Usuario el UserDetails, se importan todos sus metodos y se modifican segun lo conveniente, se CORRIGE el metodo publico que indicaba como se encripta la clave, y se CORRIGE el error de validacion del controller por que no estaba recibiendo la informacion del body en insomnia ni validandola

--- src/main/java/med/voll/api/controller/AutenticacionController.java
+++ src/main/java/med/voll/api/controller/AutenticacionController.java
@@ -1,11 +1,13 @@
 package med.voll.api.controller;
 
+import jakarta.validation.Valid;
 import med.voll.api.domain.usuarios.DatosAutenticacionUsuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -19,7 +21,7 @@ public class AutenticacionController {
     private AuthenticationManager autenticationManager;
 
     @PostMapping
-    public ResponseEntity autenticarUsuario(DatosAutenticacionUsuario datosAutenticacionUsuario){
+    public ResponseEntity autenticarUsuario(@RequestBody @Valid DatosAutenticacionUsuario datosAutenticacionUsuario){
         Authentication token = new UsernamePasswordAuthenticationToken(datosAutenticacionUsuario.login(),
                 datosAutenticacionUsuario.clave());
         autenticationManager.authenticate(token);

53de5147e7c239e9c603d5e52d95ac6dfb1e60b8
juanse951 <juanse951@gmail.com>
2024-12-22 22:26:06 -0500

    se vuelve ResponseEntity el putmapping

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -43,9 +43,15 @@ public class PacienteController {
 
     @PutMapping
     @Transactional
-    public void actualizar(@RequestBody @Valid DatosActualizacionPaciente datos){
+    public ResponseEntity actualizar(@RequestBody @Valid DatosActualizacionPaciente datos){
         Paciente paciente = pacienteRepository.getReferenceById(datos.id());
         paciente.actualizarInformacion(datos);
+
+        return ResponseEntity.ok(new DatosRespuestaPaciente(paciente.getId(), paciente.getNombre(), paciente.getEmail(),
+                paciente.getTelefono(), paciente.getDocumentoIdentidad().toString(),
+                new DatosDireccion(paciente.getDireccion().getCalle(),paciente.getDireccion().getDistrito(),
+                        paciente.getDireccion().getCiudad(),paciente.getDireccion().getNumero(),
+                        paciente.getDireccion().getComplemento())));
     }
 
     @DeleteMapping("/{id}")

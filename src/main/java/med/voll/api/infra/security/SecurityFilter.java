2bb5801d4ff8672cc2240a918347f023dd1ddbb2
juanse951 <juanse951@gmail.com>
2024-12-24 17:54:54 -0500

    06 Obteniendo el Token #2 agregamos el tratamiento de posibles errores

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -15,7 +15,12 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         //System.out.println("El filtro está siendo llamado");
-        var token = request.getHeader("Authorization").replace("Bearer ","");
+        var token = request.getHeader("Authorization");
+        if(token == null || token == ""){
+            throw new RuntimeException("El token enviado no es valido");
+        }
+        token = token.replace("Bearer ","");
+
         System.out.println(token);
         filterChain.doFilter(request,response);// unica forma de llamar al filtro
     }

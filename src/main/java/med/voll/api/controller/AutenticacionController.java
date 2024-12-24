76ffd890d22239470ff52c5fecd96b8e22837dac
juanse951 <juanse951@gmail.com>
2024-12-23 19:54:10 -0500

    se crea un dto para mostrar de dorma mas amigable el token // y se crea variable de entorno para no dejar a la vista la apikeysecret

--- src/main/java/med/voll/api/controller/AutenticacionController.java
+++ src/main/java/med/voll/api/controller/AutenticacionController.java
@@ -3,6 +3,7 @@ package med.voll.api.controller;
 import jakarta.validation.Valid;
 import med.voll.api.domain.usuarios.DatosAutenticacionUsuario;
 import med.voll.api.domain.usuarios.Usuario;
+import med.voll.api.infra.security.DatosJWTtoken;
 import med.voll.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -31,6 +32,6 @@ public class AutenticacionController {
                 datosAutenticacionUsuario.clave());
         var usuarioAutenticado = autenticationManager.authenticate(authToken);
         var JWTtoken = tokenService.generarToken((Usuario) usuarioAutenticado.getPrincipal());
-        return ResponseEntity.ok(JWTtoken);
+        return ResponseEntity.ok(new DatosJWTtoken(JWTtoken));
     }
 }
--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -4,6 +4,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import med.voll.api.domain.usuarios.Usuario;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -13,9 +14,12 @@ import java.time.ZoneOffset;
 @Service
 public class TokenService {
 
+    @Value("${api.security.secret}")
+    private String apiSecret;
+
     public String generarToken(Usuario usuario){
         try {
-            Algorithm algorithm = Algorithm.HMAC256("123456");
+            Algorithm algorithm = Algorithm.HMAC256(apiSecret);
             return JWT.create()
                     .withIssuer("voll med")
                     .withSubject(usuario.getLogin())

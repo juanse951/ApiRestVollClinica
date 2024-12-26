1e281b6330f2a61fe701411db9d3d7c053839e6e
juanse951 <juanse951@gmail.com>
2024-12-25 20:18:04 -0500

    06 Clase Service// ReservaDeConsultas donde vamos a implementar todas las reglas de negocio

--- src/main/java/med/voll/api/controller/ConsultaController.java
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -4,19 +4,29 @@ import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.domain.consulta.DatosDetalleConsulta;
 import med.voll.api.domain.consulta.DatosReservaConsulta;
+import med.voll.api.domain.consulta.ReservaDeConsultas;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+//se encarga de que el flujo de datos este correcto,recibe unas request
+//llama algo, ejemplo reglas de negocio y devuelve datos
 
 @RestController
 @RequestMapping("/consultas")
 public class ConsultaController {
 
+    @Autowired
+    private ReservaDeConsultas reserva;
+
     @PostMapping
     @Transactional
     public ResponseEntity reservar(@RequestBody @Valid DatosReservaConsulta datos){//lo que recibimos de la api
+
+        reserva.reservar(datos);
+
         return ResponseEntity.ok(new DatosDetalleConsulta(null,null,null,null));//lo que devolvemos
     }
 }
--- src/main/java/med/voll/api/domain/consulta/DatosReservaConsulta.java
+++ src/main/java/med/voll/api/domain/consulta/DatosReservaConsulta.java
@@ -1,7 +1,6 @@
 package med.voll.api.domain.consulta;
 
 import jakarta.validation.constraints.Future;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;

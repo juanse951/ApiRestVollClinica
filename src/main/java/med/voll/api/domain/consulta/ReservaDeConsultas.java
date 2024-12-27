3dcd91b2615277b6ab085de094fbc5df130f0edc
juanse951 <juanse951@gmail.com>
2024-12-26 23:00:59 -0500

    se implemente el patron strategy con la inyeccion @Autowired  private List<ValidadorDeConsultas> validadores;//03 Aplicando principios SOLID

--- src/main/java/med/voll/api/domain/consulta/ReservaDeConsultas.java
+++ src/main/java/med/voll/api/domain/consulta/ReservaDeConsultas.java
@@ -2,11 +2,15 @@ package med.voll.api.domain.consulta;
 
 import jakarta.validation.Valid;
 import med.voll.api.domain.ValidacionException;
+import med.voll.api.domain.consulta.validaciones.ValidadorDeConsultas;
 import med.voll.api.domain.medico.Medico;
 import med.voll.api.domain.medico.MedicoRepository;
 import med.voll.api.domain.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.SpringVersion;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ReservaDeConsultas {
@@ -20,6 +24,9 @@ public class ReservaDeConsultas {
     @Autowired
     private ConsultaRepository consultaRepository;
 
+    @Autowired
+    private List<ValidadorDeConsultas> validadores;
+
     public void reservar(DatosReservaConsulta datos){
 
         if(!pacienteRepository.existsById(datos.idPaciente())){
@@ -31,6 +38,7 @@ public class ReservaDeConsultas {
         }
 
         //validaciones
+        validadores.forEach(v-> v.validar(datos));
 
         var medico = elegirMedico(datos);
         var paciente = pacienteRepository.findById(datos.idPaciente()).get();

111e468fe404aafa7dbda5951243f2ee14ef0373
juanse951 <juanse951@gmail.com>
2024-12-31 16:36:50 -0500

    se crea un método para probar el escenario del error 400

--- src/test/java/med/voll/api/controller/MedicoControllerTest.java
+++ src/test/java/med/voll/api/controller/MedicoControllerTest.java
@@ -3,13 +3,20 @@ package med.voll.api.controller;
 import med.voll.api.domain.medico.DatosRegistroMedico;
 import med.voll.api.domain.medico.DatosRespuestaMedico;
 import med.voll.api.domain.medico.MedicoRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -27,5 +34,17 @@ class MedicoControllerTest {
 
     @MockBean
     private MedicoRepository repository;
+
+    @Test
+    @DisplayName("Debería devolver código http 400 cuando las informaciones son inválidas")
+    @WithMockUser
+    void registrar_escenario1() throws Exception {
+        var response = mvc
+                .perform(post("/medicos"))
+                .andReturn().getResponse();
+
+        assertThat(response.getStatus())
+                .isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
 
 }
